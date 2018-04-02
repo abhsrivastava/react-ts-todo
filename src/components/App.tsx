343ee82336e52cda28ab38309746d0b1d467eef0
Abhishek Srivastava <abhsrivastava@researchnow.com>
2018-04-01 19:47:51 -0500

    bind method inline

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,9 +7,6 @@ import { Task } from './Models';
 export class App extends React.Component<IProps, IState> {
    constructor(props: IProps) {
       super(props);
-      this.handleAdd = this.handleAdd.bind(this);
-      this.handleChange = this.handleChange.bind(this);
-      this.handleDelete = this.handleDelete.bind(this);
       this.state = {
          currentTask: '',
          taskList: []
@@ -47,8 +44,8 @@ export class App extends React.Component<IProps, IState> {
       return (
          <div>
             <h1>React Typescript Todo List</h1>
-            <TaskForm text={this.state.currentTask} handleAdd={this.handleAdd} handleChange={this.handleChange} />
-            <TaskTable taskList={this.state.taskList} handleDelete={this.handleDelete} />
+            <TaskForm text={this.state.currentTask} handleAdd={this.handleAdd.bind(this)} handleChange={this.handleChange.bind(this)} />
+            <TaskTable taskList={this.state.taskList} handleDelete={this.handleDelete.bind(this)} />
          </div>
       )
    }
